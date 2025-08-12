e6c582c26f9be8ebd0a50bfb7be37300af670d0b
vishal gupta <vishalguptagulab4321@gmail.com>
2025-08-12 11:45:27 +0530

    Update connections.php

--- inventory/components/connections.php
+++ inventory/components/connections.php
@@ -1,17 +1,12 @@
 <?php
-$servername = "localhost"; // or 127.0.0.1
-$username = "root";        // your DB username
-$password = "";            // your DB password ("" for default XAMPP)
-$dbname = "inventory";     // your database name
+$servername = getenv("DB_HOST");
+$username   = getenv("DB_USER");
+$password   = getenv("DB_PASS");
+$dbname     = getenv("DB_NAME");
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// Optional: Uncomment below to confirm connection
-//echo "Connected successfully!";
 ?>
