a75bab1fb526ab277afd92288580c28cacb7db28
vishal gupta <vishalguptagulab4321@gmail.com>
2025-08-12 12:51:29 +0530

    Update connections.php

--- inventory/components/connections.php
+++ inventory/components/connections.php
@@ -1,11 +1,9 @@
 <?php
-$servername = getenv("DB_HOST") ?: "mysql";
+$servername = getenv("DB_HOST") ?: "localhost";  // Changed from "mysql"
 $username   = getenv("DB_USER") ?: "root";
 $password   = getenv("DB_PASS") ?: "secret";
 $dbname     = getenv("DB_NAME") ?: "inventory";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
